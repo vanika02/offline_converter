b20e83be0d2be2ac04984cc037686ddb440551b3
vanika02 <vanikadangi02@gmail.com>
2026-05-26 04:07:36 +0000

    basic img to pdf converter

--- app/convertors/image_to_pdf.py
+++ app/convertors/image_to_pdf.py
@@ -7,7 +7,28 @@ import os
 # img.save("out.pdf", save_all=True)
 
 # storing image path
-img_path = "C:\Users\Prans\OneDrive\Pictures"
+img_path = "C:\Users\Prans\Downloads\img.jpeg"
 
 # storing pdf path
-pdf_path = "C:\Users\Prans\Downloads"
+pdf_path = "C:\Users\Prans\Downloads\file.pdf"
+
+# opening image
+image = Image.open(img_path)
+
+# converting into chunks using img2pdf
+pdf_bytes = img2pdf.convert(image.filename)
+
+# opening or creating pdf file 
+file = open(pdf_path, "wb")
+
+# writing pdf files with chunks 
+file.write(pdf_bytes)
+
+# closing image file 
+image.close()
+
+# closing pdf file 
+file.close()
+
+# output
+print("Successfully made pdf file")
