f76bca7d207386c2ae3b77c2bdd9426f0a43d49a
vanika02 <vanikadangi02@gmail.com>
2026-05-26 03:49:53 +0000

    path added for image and the generated pdf

--- app/convertors/image_to_pdf.py
+++ app/convertors/image_to_pdf.py
@@ -1,4 +1,13 @@
 from PIL import Image
+import img2pdf
+import os
 
-img = PIL.Image.open("pic.jpg").convert("RGB")
-img.save("out.pdf", save_all=True)
+
+# img = PIL.Image.open("pic.jpg").convert("RGB")
+# img.save("out.pdf", save_all=True)
+
+# storing image path
+img_path = "C:\Users\Prans\OneDrive\Pictures"
+
+# storing pdf path
+pdf_path = "C:\Users\Prans\Downloads"
