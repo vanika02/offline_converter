189bd74b6e82b61dbfc4677d7caf279e67d024c5
vanika02 <vanikadangi02@gmail.com>
2026-05-27 15:01:48 +0000

    updated to func for image to pdf

--- app/convertors/image_to_pdf.py
+++ app/convertors/image_to_pdf.py
@@ -2,35 +2,14 @@ from PIL import Image
 import img2pdf
 import os
 
+def convert_image_to_pdf(img_path, pdf_path):
+    image = Image.open(img_path)
+    pdf_bytes - img2pdf.convert(image.filename)
 
-# img = PIL.Image.open("pic.jpg").convert("RGB")
-# img.save("out.pdf", save_all=True)
+    with open(pdf_path, "wb") as file:
+        file.write(pdf_bytes)
 
-# storing image path
-img_path = "/mnt/c/Users/Prans/Downloads/img.jpeg"
-# print(img_path)
+    image.close()
 
-# storing pdf path
-pdf_path = "/mnt/c/Users/Prans/Downloads/imgzz.pdf"
-
-# opening image
-image = Image.open(img_path)
-
-# converting into chunks using img2pdf
-pdf_bytes = img2pdf.convert(image.filename)
-
-# opening or creating pdf file 
-file = open(pdf_path, "wb")
-
-# writing pdf files with chunks 
-file.write(pdf_bytes)
-
-# closing image file 
-image.close()
-
-# closing pdf file 
-file.close()
-
-# output
-print("Successfully made pdf file")
+    return True
 
