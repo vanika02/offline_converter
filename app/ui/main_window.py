ffa0f36aa3a4f75c4bbe88b6a18538714d1b8cd7
vanika02 <vanikadangi02@gmail.com>
2026-05-28 01:01:00 +0000

    Qpushbtton added

--- app/ui/main_window.py
+++ app/ui/main_window.py
@@ -1,7 +1,26 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication, QLabel, QPushButton
+from PySide6.QtCore import Slot
 
+
+# this creates a func that logs message to the console - slot is a decorator that identifies func as a slot
+@Slot()
+def say_hello():
+    print("Button clicked, wasssuppp")
+
+# this will create QApplication to run the pyside6 code
 app = QApplication(sys.argv)
-label = QLabel("Hello World!")
-label.show()
+
+
+# create a button, connect and show it
+button = QPushButton("Click me")
+button.clicked.connect(say_hello)
+button.show()
+
+
+# label = QLabel("Hello World!")
+# label.show()
+
+
+# run the main Qt Loop
 app.exec()
