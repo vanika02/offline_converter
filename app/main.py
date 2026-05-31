0286c848e06c063134ad3b46cd64073c74956714
vanika02 <vanikadangi02@gmail.com>
2026-05-31 01:18:36 +0000

    app instance created

--- app/main.py
+++ app/main.py
@@ -2,3 +2,9 @@ from PySide6.QtWidgets import QApplication, QWidget
 
 # need this one for access to the command line arguments
 import sys
+
+
+# we need one and onl yone QApplication instance per application
+# Pass in sys.argv to allow command line arguments for your app
+# If you know you won't use command line arguments QApplication([]) works too.
+app = QApplication(sys.argv)
