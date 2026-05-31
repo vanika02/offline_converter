dc6f1eb2fbeb87b984a6b25859e8983f1e8d44fe
vanika02 <vanikadangi02@gmail.com>
2026-05-31 01:21:31 +0000

    main window instance created

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from PySide6.QtWidgets import QApplication, QWidget
+from app.ui.main_window import MainWindow
+
 
 # need this one for access to the command line arguments
 import sys
@@ -8,3 +10,6 @@ import sys
 # Pass in sys.argv to allow command line arguments for your app
 # If you know you won't use command line arguments QApplication([]) works too.
 app = QApplication(sys.argv)
+
+# this creates an instance of the main window
+window = MainWindow()
