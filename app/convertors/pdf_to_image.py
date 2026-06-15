db8222e922f56ed09dc0d240e423eb58c69ccd8a
vanika02 <vanikadangi02@gmail.com>
2026-06-15 01:41:22 +0000

    pdf to image function corrected

--- app/convertors/pdf_to_image.py
+++ app/convertors/pdf_to_image.py
@@ -1,13 +1,9 @@
 from PIL import Image
 from pdf2image import convert_from_path
 
-def convert_pdf_to_image(pdf_path, img_path)
-    pdf = Image.open(pdf_path)
-    image_bytes = convert_from_path(pdf.filename)
+def convert_pdf_to_image(pdf_path, img_path):
+    image = convert_from_path(pdf_path)
 
-    with open(img_path, "wb") as file:
-        file.write(image_bytes)
-    
-    image.close()
+    image[0].save(img_path)
 
     return True
