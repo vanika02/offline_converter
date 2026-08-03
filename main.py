7d89d32f106235ef8a9438d4760d47c3d8b4542a
vanika02 <vanikadangi02@gmail.com>
2026-08-03 11:15:27 +0000

    Refactor: created main function in main.py

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from PySide6.QtWidgets import QApplication, QWidget
 from app.ui.main_window import MainWindow
-
-
-# need this one for access to the command line arguments
 import sys
 
+def main():
+    app = QApplication(sys.argv)
 
-# we need one and onl yone QApplication instance per application
-# Pass in sys.argv to allow command line arguments for your app
-# If you know you won't use command line arguments QApplication([]) works too.
-app = QApplication(sys.argv)
+    window = MainWindow()
 
-# this creates an instance of the main window
-window = MainWindow()
+    window.show()
 
-window.show() # shows the window (widgets are hidden by default)
+    app.exec()
 
-# start the event loop
-app.exec()
+if __name__ == "__main__":
+    main()
+    
