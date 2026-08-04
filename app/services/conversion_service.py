e8865d30416f1ed1dcc157bc51bc27a2fa7fa926
vanika02 <vanikadangi02@gmail.com>
2026-08-04 08:07:46 +0000

    fix: import and wrong variable issue resolved

--- app/services/conversion_service.py
+++ app/services/conversion_service.py
@@ -1,5 +1,6 @@
 from app.convertors.image_to_pdf import convert_image_to_pdf
 from app.convertors.pdf_to_image import convert_pdf_to_image
+import os
 
 class ConversionService:
 
@@ -9,33 +10,22 @@ class ConversionService:
             raise ValueError("Please select a file")
             return 
         
-        output_dir = SaveFileDialog()
-
         if not output_dir:
             raise ValueError("No output directory selected")
-            return 
-        
-        conversion = conversion_box.currentText()
+            return      
 
-        try: 
-            if conversion == "Image -> PDF":
-                pdf_path = os.path.join(
-                    output_dir, f"{image_name}.pdf"
-                )
-
-                convert_image_to_pdf(self.input_file, pdf_path)
-            
-            elif conversion == "PDF -> Image":
-
-                convert_pdf_to_image(self.input_file, output_dir)
-
-            if True:
-                QMessageBox.information(
-                    self, "Success", f"PDF created successfully!\n\n{pdf_path}"
-                )
-            
-        except Exception as e:
-            QMessageBox.critical(
-                self, "Conversion failed", str(e)
+        if conversion_type == "Image -> PDF":
+            pdf_path = os.path.join(
+                output_dir, f"{image_name}.pdf"
             )
+
+            convert_image_to_pdf(self.input_file, pdf_path)
+        
+        elif conversion_type == "PDF -> Image":
+
+            convert_pdf_to_image(self.input_file, output_dir)
+
+        else:
+            raise ValueError("Unsupported converison")
+
 
