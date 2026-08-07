046cd02c971c148fbd7077f042d244cd910ace74
vanika02 <vanikadangi02@gmail.com>
2026-08-07 04:04:20 +0000

    feat: converted image to pdf function into abstract class

--- app/convertors/image_to_pdf.py
+++ app/convertors/image_to_pdf.py
@@ -3,26 +3,28 @@ from pathlib import Path
 from PIL import Image
 import img2pdf
 
+class ImageToPdfConverter(BaseConverter):
 
-def convert_image_to_pdf(input_file: str, output_dir: str) -> str:
-    """
-    Convert an image to PDF.
 
-    Args:
-        input_file: Path to the input image.
-        output_dir: Directory where the PDF should be saved.
-    
-    Returns:
-        Path to the created PDF.
-    """
+    def convert(input_file: str, output_dir: str) -> str:
+        """
+        Convert an image to PDF.
 
-    image_name = Path(input_file).stem
-    output_path = Path(output_dir) / f"{image_name}.pdf"
+        Args:
+            input_file: Path to the input image.
+            output_dir: Directory where the PDF should be saved.
+        
+        Returns:
+            Path to the created PDF.
+        """
 
-    with Image.open(input_file) as image:
-        pdf_bytes = img2pdf.convert(image.filename)
-    
-    with open(output_path, "wb") as file:
-        file.write(pdf_bytes)
+        image_name = Path(input_file).stem
+        output_path = Path(output_dir) / f"{image_name}.pdf"
 
-    return str(output_path)
+        with Image.open(input_file) as image:
+            pdf_bytes = img2pdf.convert(image.filename)
+        
+        with open(output_path, "wb") as file:
+            file.write(pdf_bytes)
+
+        return str(output_path)
