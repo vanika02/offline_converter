fac453bb0591e72928289f9de2951773e28c69aa
vanika02 <vanikadangi02@gmail.com>
2026-08-08 05:28:14 +0000

    feat: initialized setup_logging in main application startup

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from PySide6.QtWidgets import QApplication, QWidget
+from app.logging_config import setup_logging
 from app.ui.main_window import MainWindow
 import sys
 
 def main():
+
+    setup_logging()
+
     app = QApplication(sys.argv)
 
     window = MainWindow()
