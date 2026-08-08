928966f95b162ef91f8830345d3bc3b83dd646ec
vanika02 <vanikadangi02@gmail.com>
2026-08-08 05:52:20 +0000

    conversion service logs created

--- app/services/conversion_service.py
+++ app/services/conversion_service.py
@@ -1,7 +1,10 @@
 from app.convertors.image_to_pdf import ImageToPdfConverter
 from app.convertors.pdf_to_image import PdfToImageConverter
 from app.enums.conversion_type import ConversionType
+import logging
 import os
+
+logger = logging.getLogger(__name__) # name gives the module's name
 
 class ConversionService:
 
@@ -18,6 +21,21 @@ class ConversionService:
         output_dir: str, 
         conversion_type: ConversionType) -> str:
 
+        logger.info(
+            "Starting conversion: %s",
+            conversion_type,
+        )
+
+        logger.debug(
+            "Input file: %s",
+            input_file,
+        )
+
+        logger.debug(
+            "Output directory: %s",
+            output_dir,
+        )
+
         if not input_file:
             raise ValueError("Please select a file")
         
@@ -29,6 +47,16 @@ class ConversionService:
         if converter is None:
             raise ValueError("Unsupported conversion type")
         
+
         return converter.convert(input_file, output_dir)
 
+        logger.info(
+            "Conversion completed successfully: %s",
+            output_files,
+        )
+
+        logger.error(
+            "Conversion failed",
+            "exec_info=True"
+        )
 
